b1b5ce93ddb8869c2f3b94f903b71b46cb947db4
sauta <sauta@google.com>
2010-05-17 12:23:43 +0000

    Rakefiles to make DerivedSources and use dependency files for HLWK.

--- hlwk/WebCore/DerivedSources/UserAgentStyleSheets.h
+++
@@ -1,9 +0,0 @@
-namespace WebCore {
-extern const char htmlUserAgentStyleSheet[9152];
-extern const char quirksUserAgentStyleSheet[359];
-extern const char sourceUserAgentStyleSheet[2022];
-extern const char themeWinUserAgentStyleSheet[912];
-extern const char themeWinQuirksUserAgentStyleSheet[34];
-extern const char svgUserAgentStyleSheet[351];
-extern const char mediaControlsUserAgentStyleSheet[2813];
-}
